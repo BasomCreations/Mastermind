6af7ecba80897dab7a053673df7bdf2c870d139b
Jonathan Basom <jab108@bucknell.edu>
2019-10-06 22:29:34 +0100

    Initial commit

---
+++ src/main/main.java
@@ -0,0 +1,25 @@
+/* *****************************************
+ * CSCI205 - Software Engineering and Design
+ * Fall 2019
+ * Instructor: Prof. Brian King
+ *
+ * Name: Jonathan Basom
+ * Section: 9am
+ * Date: 10/6/2019
+ * Time: 5:18 PM
+ *
+ * Project: csci205_hw
+ * Package: main
+ * Class: main
+ *
+ * Description:
+ *
+ * ****************************************
+ */
+package main;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+}
