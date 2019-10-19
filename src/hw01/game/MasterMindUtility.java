fe80ba1d8f417e69671c7bac9fce1d7c64517a94
jab108 <jab108@bucknell.edu>
2019-10-19 16:44:53 -0400

    Merge branch 'master' of C:\Users\jbaso\Documents\Bucknell\Fall 2019\csci205_hw with conflicts.

--- src/hw01/game/MasterMindUtility.java
+++ src/hw01/game/MasterMindUtility.java
@@ -18,8 +18,11 @@
  */
 package hw01.game;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 /**
  * Utility class for useful functions
@@ -127,4 +130,72 @@ public final class MasterMindUtility {
         }
         return secretCode;
     }
+
+    /**
+     * Verify that user enters a valid number
+     * @param in Scanner object
+     * @return int that is the valid number
+     * @author Jonathan
+     */
+    public static int verifyNumericInput(Scanner in) {
+        while (true) {
+            checkInt: if (in.hasNextInt()) {
+                int number = in.nextInt();
+                if (number <= 0) {
+                    System.out.println("Must be greater than 0 - Try Again:");
+                    in.nextLine();
+                    break checkInt;
+                }
+                return number;
+            }
+            else {
+                System.out.println("Error! Please enter numeric value.");
+                in.nextLine();
+            }
+        }
+    }
+
+
+    /**
+     * Returns a Row object for the results of a guess
+     * @param thisguess guess made
+     * @param code secret code
+     * @return
+     */
+    public static Row makeGuess(int[] thisguess, int[]  code){
+        int[] guesses = thisguess.clone();
+        int[] secretCode = code.clone();
+
+        if(guesses.length != secretCode.length){
+            throw new IllegalArgumentException("You cant do that!");
+        }
+
+        List<Integer> guessList = Arrays.stream(secretCode).boxed().collect(Collectors.toList());
+
+        int correctPegs = 0;
+        for (int i = 0; i < guesses.length; i++) {
+            if (guesses[i] == secretCode[i]) {
+                correctPegs++;
+                //Cast to object to make sure it removes the actual value instead of the index
+                guessList.remove((Object) guesses[i]);
+                guesses[i] = -1;
+            }
+        }
+
+        int pegsIncorrectPosition = 0;
+        for (int guess:
+                guesses) {
+            if (guessList.contains(guess)){
+                pegsIncorrectPosition++;
+                //Cast to object to make sure it removes the actual value instead of the index
+                guessList.remove((Object)guess);
+            }
+        }
+
+        return new Row(correctPegs, pegsIncorrectPosition, MasterMindBoard.ROW_SIZE - correctPegs - pegsIncorrectPosition);
+
+
+    }
+
+
 }
---
+++ src/hw01/solver/RandomSolver.java
@@ -0,0 +1,47 @@
+/* *****************************************
+ * CSCI205 - Software Engineering and Design
+ * Fall 2019
+ * Instructor: Prof. Brian King
+ *
+ * Name: Jonathan Basom / Sebastian Ascoli
+ * Section: 9am / 11 am
+ * Date: 10/17/2019
+ * Time: 1:51 PM
+ *
+ * Project: csci205_hw
+ * Package: hw01.solver
+ * Class: RandomSolver
+ *
+ * Description:
+ *
+ * ****************************************
+ */
+package hw01.solver;
+
+import hw01.game.MasterMindBoard;
+import hw01.game.MasterMindBoardException;
+import hw01.game.MasterMindUtility;
+
+public class RandomSolver extends Solver {
+
+//    public RandomSolver() {
+//        super();
+//    }
+
+    @Override
+    protected int[] getNextMove() {
+        return MasterMindUtility.generateRandomSecretCode();
+    }
+
+    @Override
+    protected int play() throws MasterMindBoardException {
+        MasterMindBoard board = new MasterMindBoard(true);
+        while (!board.checkWin()) {
+            board.guess(this.getNextMove());
+        }
+        return board.getGuesses();
+    }
+
+
+
+}
