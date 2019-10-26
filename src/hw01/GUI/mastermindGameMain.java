5ddf96ecf00420fc9f246dc0bd8752cf53607bf1
Sebastian Ascoli <sa036@bucknell.edu>
2019-10-26 12:28:37 -0400

    Added icons

--- src/hw01/GUI/mastermindGameMain.java
+++ src/hw01/GUI/mastermindGameMain.java
@@ -2,6 +2,9 @@ package hw01.GUI;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -14,15 +17,24 @@ public class mastermindGameMain extends Application {
     @Override
     public void start(Stage primaryStage) {
 
-        //Set up root node for our scene graph
         StackPane root = new StackPane();
-
-        // Add the scene graph to the stage
         primaryStage.setScene(new Scene(root, 400, 300));
+        primaryStage.setTitle("Mastermind");
+        primaryStage.getIcons().add(new Image("file:images/icon1.png"));
 
-        // Set the title for the main window
-        primaryStage.setTitle("Hello, JavaFX!");
 
+
+        StackPane layout2 = new StackPane();
+        Scene scene2 = new Scene(layout2,400,300);
+        Label lb1 = new Label("Welcome ");
+        layout2.getChildren().add(lb1);
+
+
+        Button btn1 = new Button("Scene1");
+        btn1.setOnAction(event -> primaryStage.setScene(scene2));
+        root.getChildren().add(btn1);
+
+        
 
         // Display the scene
         primaryStage.show();
